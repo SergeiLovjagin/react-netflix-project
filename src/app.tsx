d337691372f779d9af0d346fff9d9606eb7a17ba
SergeiLovjagin <serlovjagin@gmail.com>
2021-07-14 10:18:01 +0300

    Eslint config. Created JumbotronContainer.

---
+++ src/app.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { JumbotronContainer } from './containers/JumbotronContainer';
+
+export const App: React.FC = () => (
+  <JumbotronContainer />
+);
---
+++ src/components/jumbotron/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  Container, Image, Inner, Item, Pane, SubTitle, Title,
+} from './styles/jumbotron';
+
+type JumbotronProps = {
+  children: any
+  direction: string
+}
+
+export const Jumbotron = ({ children, direction = 'row', ...restProps }: JumbotronProps): JSX.Element => (
+  <Item {...restProps}>
+    <Inner direction={direction}>
+      {children}
+    </Inner>
+  </Item>
+);
+
+Jumbotron.Container = ({ children, ...restProp }: { children: JSX.Element | JSX.Element[] | string }): JSX.Element => <Container {...restProp}>{children}</Container>;
+
+Jumbotron.Pane = ({ children, ...restProp }: { children: JSX.Element | JSX.Element[] | string }): JSX.Element => <Pane {...restProp}>{children}</Pane>;
+
+Jumbotron.Title = ({ children, ...restProp }: { children: JSX.Element | JSX.Element[] | string }): JSX.Element => <Title {...restProp}>{children}</Title>;
+
+Jumbotron.SubTitle = ({ children, ...restProp }: { children: JSX.Element | JSX.Element[] | string }): JSX.Element => <SubTitle {...restProp}>{children}</SubTitle>;
+
+Jumbotron.Image = ({ children, ...restProp }: any) => <Image {...restProp}>{children}</Image>;
